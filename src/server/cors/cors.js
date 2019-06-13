626e700d52ae4892ae8947f70bdb8d641717f79c
Erlend Vige <erlend.vige@gmail.com>
2019-06-13 15:27:51 +0200

    redirect to referer, remove unused /login endpoint

--- src/server/cors/cors.js
+++ src/server/cors/cors.js
@@ -1,10 +1,11 @@
 export default (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, Content-Type, X-AUTHENTICATION, X-IP, Content-Type, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With'
     );
+    res.setHeader('Access-Control-Expose-Headers', 'Location');
     return next();
 };
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,31 +1,18 @@
-import { isAuthenticated } from '../utils/auth';
-
 const configRoutes = (app, authClient) => {
-    app.get('/login', (req, res) => {
-        if (!isAuthenticated(req.session.tokenSets)) {
-            const authorizationUrl = authClient.authorizationUrl({
-                response_mode: 'form_post',
-                scope: `openid ${process.env.CLIENT_ID}/.default`
-            });
-            res.redirect(authorizationUrl);
-        } else {
-            res.send('Hei på deg!');
-        }
-    });
-
     app.post('/callback', (req, res) => {
         const authorizationCode = req.query.code;
         const params = authClient.callbackParams(req);
         authClient
             .callback(process.env.REDIRECT_URL, params, {
-                code_verifier: authorizationCode
+                code_verifier: authorizationCode,
+                state: req.body.state
             })
             .then(
                 tokenSet => {
                     req.session.tokenSets = {
                         [process.env.CLIENT_ID]: tokenSet
                     };
-                    res.redirect(req.session.referer);
+                    res.redirect(req.body.state);
                 },
                 error => {
                     console.error(error);

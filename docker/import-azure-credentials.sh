86604f1d4d86dfe1e062c5fe96e1cfb6ded280c3
Aslak Hollund <aslak.hollund@nav.no>
2020-08-04 16:13:43 +0200

    update import azure credentials with a second path in case of using the new azure provisioning

--- docker/import-azure-credentials.sh
+++ docker/import-azure-credentials.sh
@@ -2,6 +2,8 @@
 
 echo "Importing Azure credentials"
 
+FILE_NAME=NULL
+
 if test -d /var/run/secrets/nais.io/azuread;
 then
     for FILE in /var/run/secrets/nais.io/azuread/*
@@ -13,4 +15,27 @@ then
         echo "- exporting $KEY"
         export "$KEY"="$VALUE"
     done
+fi
+
+
+if test -d /var/run/secrets/nais.io/azure;
+then
+    for FILE in /var/run/secrets/nais.io/azure/*
+    do
+        FILE_NAME=$(echo $FILE | sed 's:.*/::')
+        KEY=$(echo $FILE_NAME | tr '[:lower:]' '[:upper:]')
+        VALUE=$(cat "$FILE")
+
+        echo "- exporting $KEY"
+        export "$KEY"="$VALUE"
+    done
+
+    FILE_NAME=/var/run/secrets/nais.io/azure/AZURE_APP_JWKS
+    VALUE=$(cat $FILE_NAME | jq '.keys[0]')
+
+    export CLIENT_ID=$AZURE_APP_CLIENT_ID
+    export DISCOVERY_URL=$AZURE_APP_WELL_KNOWN_URL
+    export PRE_AUTHORIZED_APPS=$AZURE_APP_PRE_AUTHORIZED_APPS
+    export CLIENT_SECRET=$AZURE_APP_CLIENT_SECRET
+    export JWK=$VALUE
 fi

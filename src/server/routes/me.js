6dbb1f24b512ab31036cae3956616976f11f9b8f
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-11-25 12:26:58 +0100

    Oppdatert Jose Major update.

--- src/server/routes/me.js
+++ src/server/routes/me.js
@@ -1,12 +1,12 @@
 import { isAuthenticated, getTokenSetsFromSession } from '../utils/auth';
-import { JWT } from 'jose';
+import jwt_decode from 'jwt-decode';
 
 export const meRoutes = (app) => {
     app.get('/me', function (request, response) {
         const authenticated = isAuthenticated({ request });
         if (authenticated) {
             const tokenSet = getTokenSetsFromSession({ request })['self'];
-            const decodedIdToken = JWT.decode(tokenSet.id_token);
+            const decodedIdToken = jwt_decode(tokenSet.id_token);
             if (decodedIdToken.name) {
                 response.json({
                     name: decodedIdToken.name,

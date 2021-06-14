8b8075ef18e45c822b81fba9fa154b31389ef6c7
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-06-14 11:53:35 +0200

    Breaking change å hente config på. Fjerner Azure/IdPorten spesifikke filer.

--- src/server/utils/config.js
+++ src/server/utils/config.js
@@ -1,37 +1,72 @@
 import logger from './log';
+import fs from 'fs';
 
-const environmentVariable = ({ name, secret = false, required = true }) => {
-    if (!process.env[name] && required) {
-        logger.error(`Mangler environment variable '${name}'.`);
+const ENV_PREFIX = "env:"
+const PATH_PREFIX = "path:"
+const VALUE_PREFIX = "value:"
+
+const configValue = ({name, secret = false, required = true}) => {
+    // Finner ut hvor vi skal lete etter config
+    const pointer = process.env[name];
+    if (!pointer && required) {
+        logger.error(`Config: Mangler environment variable ${name}`);
         process.exit(1);
+    } else if (!pointer) {
+        logger.info(`Config: Optional ${name} ikke satt.`);
+        return null;
     }
-    if (!secret) {
-        logger.info(`Env[${name}]=${process.env[name]}`);
+
+    // Setter configverdi
+    let value = null;
+    if (pointer.startsWith(ENV_PREFIX)) {
+        value = process.env[pointer.slice(ENV_PREFIX.length)];
+    } else if (pointer.startsWith(PATH_PREFIX)) {   
+        value = fs.readFileSync(pointer.slice(PATH_PREFIX.length), 'utf-8');
+    } else if (pointer.startsWith(VALUE_PREFIX)) {
+        value = pointer.slice(VALUE_PREFIX.length);
     } else {
-        logger.info(`Env[${name}]=***`);
+        logger.error(`Config: Environment variable ${name} må peke på en verdi som starter med ${ENV_PREFIX}, ${PATH_PREFIX}, ${VALUE_PREFIX}`);
+        process.exit(1);    
     }
-    return process.env[name];
+
+    // Validerer
+    if (!value && required) {
+        logger.error(`Config: Mangler verdi på ${pointer}`);
+        process.exit(1);
+    } else if (!value) {
+        logger.info(`Config: Optional ${name} ikke satt.`);
+        return null;
+    }
+
+    // Logger
+    if (secret) {
+        if (pointer.startsWith(VALUE_PREFIX)) {
+            logger.info(`Config: ${name}=*** (hentet fra ${VALUE_PREFIX}***)`);
+        } else {
+            logger.info(`Config: ${name}=*** (hentet fra ${pointer})`);
+        }
+    } else {
+        logger.info(`Config: ${name}=${value} (hentet fra ${pointer})`);
+    }
+
+    return value;
 };
 
-const environmentVariableAsJson = ({ name, secret = false, required = true }) => {
-
-    let env = environmentVariable({ name, secret, required });
-    if (!env && !required) {
-        logger.info(`Valgfri environment variable '${name}' er ikke satt.`);
-        return undefined
-    }
+const configValueAsJson = ({ name, secret = false, required = true }) => {
+    const value = configValue({ name, secret, required });
+    if (!value) { return null; }
     try {
-        return JSON.parse(env);
+        return JSON.parse(value);
     } catch (error) {
-        logger.error(`Environment variable '${name}' er ikke et gyldig JSON-objekt.`, error);
+        logger.error(`Config: '${name}' er ikke et gyldig JSON-objekt.`, error);
         process.exit(1);
     }
 };
 
 const getJwks = () => {
-    var jwk = environmentVariableAsJson({ name: 'JWK', secret: true });
+    const jwk = configValueAsJson({ name: 'JWK', secret: true, required: true });
     if (!jwk.kid) {
-        logger.error(`Environment variable 'JWK' mangler 'kid' claim.`);
+        logger.error(`Config: 'JWK' mangler 'kid' claim.`);
         process.exit(1);
     }
     // UnhandledPromiseRejectionWarning: JWKInvalid: `x5c` member at index 0 is not a valid base64-encoded DER PKIX certificate
@@ -42,9 +77,9 @@ const getJwks = () => {
 };
 
 const getProxyConfig = () => {
-    var config = environmentVariableAsJson({ name: 'PROXY_CONFIG' });
+    var config = configValueAsJson({ name: 'PROXY_CONFIG' });
     if (!config.apis) {
-        logger.error("Environment variable 'PROXY_CONFIG' mangler 'apis' entry.");
+        logger.error("Config: 'PROXY_CONFIG' mangler 'apis' entry.");
         exit(1);
     }
     config.apis.forEach((entry, index) => {
@@ -66,26 +101,20 @@ const getProxyConfig = () => {
 };
 
 const getAdditionalAuthorizationParameters = () => {
-    return environmentVariableAsJson(
-        {
-            name: 'ADDITIONAL_AUTHORIZATION_PARAMETERS',
-            secret: false,
-            required: false
-        });
+    return configValueAsJson({name: 'ADDITIONAL_AUTHORIZATION_PARAMETERS', secret: false, required: false});
 };
 
-const clientId = environmentVariable({ name: 'CLIENT_ID' });
-const loginScopes = environmentVariable({ name: 'LOGIN_SCOPES' });
-const discoveryUrl = environmentVariable({ name: 'DISCOVERY_URL' });
-const oidcAuthProxyBaseUrl = environmentVariable({ name: 'OIDC_AUTH_PROXY_BASE_URL' });
-const applicationBaseUrl = environmentVariable({ name: 'APPLICATION_BASE_URL' });
-const allowProxyToSelfSignedCertificates = 
-    environmentVariable({ name: 'ALLOW_PROXY_TO_SELF_SIGNED_CERTIFICATES', required: false }) === 'false'
+const clientId = configValue({ name: 'CLIENT_ID' });
+const loginScopes = configValue({ name: 'LOGIN_SCOPES' });
+const discoveryUrl = configValue({ name: 'DISCOVERY_URL' });
+const oidcAuthProxyBaseUrl = configValue({ name: 'OIDC_AUTH_PROXY_BASE_URL' });
+const applicationBaseUrl = configValue({ name: 'APPLICATION_BASE_URL' });
+const allowProxyToSelfSignedCertificates = configValue({ name: 'ALLOW_PROXY_TO_SELF_SIGNED_CERTIFICATES', required: false }) === 'false'
 const callbackPath = '/oidc/callback';
 const callbackUrl = `${oidcAuthProxyBaseUrl}${callbackPath}`;
-const sessionIdCookieName = environmentVariable({ name: 'SESSION_ID_COOKIE_NAME' });
-const sessionIdCookieSignSecret = environmentVariable({ name: 'SESSION_ID_COOKIE_SIGN_SECRET', secret: true });
-const sessionIdCookieVerifySecret = environmentVariable({ name: 'SESSION_ID_COOKIE_VERIFY_SECRET', secret: true });
+const sessionIdCookieName = configValue({ name: 'SESSION_ID_COOKIE_NAME' });
+const sessionIdCookieSignSecret = configValue({ name: 'SESSION_ID_COOKIE_SIGN_SECRET', secret: true });
+const sessionIdCookieVerifySecret = configValue({ name: 'SESSION_ID_COOKIE_VERIFY_SECRET', secret: true });
 const sessionIdCookieSecrets = [sessionIdCookieSignSecret, sessionIdCookieVerifySecret];
 
 const getSessionIdCookieProperties = () => {
@@ -114,19 +143,19 @@ const getSessionIdCookieProperties = () => {
     };
 };
 
-const useInMemorySessionStore = () => environmentVariable({ name: 'USE_IN_MEMORY_SESSION_STORE', required: false }) === 'true';
+const useInMemorySessionStore = () => configValue({ name: 'USE_IN_MEMORY_SESSION_STORE', required: false }) === 'true';
 
-const getRedisPassword = () => environmentVariable({ name: 'REDIS_PASSWORD', secret: true });
-const getRedisPort = () => environmentVariable({ name: 'REDIS_PORT' });
-const getRedisHost = () => environmentVariable({ name: 'REDIS_HOST' });
+const getRedisPassword = () => configValue({ name: 'REDIS_PASSWORD', secret: true });
+const getRedisPort = () => configValue({ name: 'REDIS_PORT' });
+const getRedisHost = () => configValue({ name: 'REDIS_HOST' });
 
 const getCorsAllowedHeaders = () => {
-    const value = environmentVariable({ name: 'CORS_ALLOWED_HEADERS', required: false });
+    const value = configValue({ name: 'CORS_ALLOWED_HEADERS', required: false });
     if (!value) return 'x-correlation-id';
     else return `${value},x-correlation-id`;
 };
 const getCorsExposedHeaders = () => {
-    const value = environmentVariable({ name: 'CORS_EXPOSED_HEADERS', required: false });
+    const value = configValue({ name: 'CORS_EXPOSED_HEADERS', required: false });
     if (!value) return [];
     else return value;
 };

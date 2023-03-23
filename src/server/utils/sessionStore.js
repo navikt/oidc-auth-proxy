5d1fc36bca48aefd6b4da8b1b39450ee332dd655
vebnor <vebjorn.nordby@nav.no>
2023-03-23 10:17:36 +0100

    oppdaterer redis

--- src/server/utils/sessionStore.js
+++ src/server/utils/sessionStore.js
@@ -1,15 +1,15 @@
 import config from './config';
 import logger from './log';
 import redis from 'redis';
+import RedisStore from 'connect-redis';
 import { MemoryStore } from 'express-session';
 
-export const getSessionStore = (session) => {
+export const getSessionStore = () => {
     if (config.useInMemorySessionStore()) {
         logger.warning('Kjører applikasjonen med Session Store In Memory.');
         return new MemoryStore();
     } else {
         logger.info('Initialiserer Redis Session Store.');
-        const RedisStore = require('connect-redis')(session);
         const redisClient = redis.createClient({
             host: config.getRedisHost(),
             password: config.getRedisPassword(),
